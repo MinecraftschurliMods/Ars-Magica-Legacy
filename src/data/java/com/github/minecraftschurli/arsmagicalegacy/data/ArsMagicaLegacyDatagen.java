d9ace488fb2f7bcc6996f659d59ab312b439aa17
Minecraftschurli <minecraftschurli@gmail.com>
2021-09-13 10:26:20 +0200

    start using patchouli_datagen

--- src/data/java/com/github/minecraftschurli/arsmagicalegacy/data/ArsMagicaLegacyDatagen.java
+++ src/data/java/com/github/minecraftschurli/arsmagicalegacy/data/ArsMagicaLegacyDatagen.java
@@ -1,13 +1,20 @@
 package com.github.minecraftschurli.arsmagicalegacy.data;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
+import com.github.minecraftschurli.patchouli_datagen.BookBuilder;
+import com.github.minecraftschurli.patchouli_datagen.PatchouliBookProvider;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.HashCache;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Consumer;
 
 /**
  * @author Minecraftschurli
@@ -28,5 +35,16 @@ public class ArsMagicaLegacyDatagen {
         if (evt.includeServer()) {
             TagsProvider.setup(generator, existingFileHelper);
         }
+        generator.addProvider(new PatchouliBookProvider(generator, ArsMagicaAPI.MOD_ID, "en_us", evt.includeClient(), evt.includeServer()) {
+            @Override
+            protected void addBooks(Consumer<BookBuilder> consumer) {
+                createBookBuilder("arcane_compendium")
+                        .setModel(new ResourceLocation(ArsMagicaAPI.MOD_ID, "arcane_compendium"))
+                        .setCreativeTab(ArsMagicaAPI.get().getItemGroup().getRecipeFolderName())
+                        .setUseResourcepack()
+                        .setUseI18n()
+                        .build(consumer);
+            }
+        });
     }
 }
