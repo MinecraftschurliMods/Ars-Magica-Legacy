189bc25ff2d69858b106f738484db1a218cd18c0
Minecraftschurli <minecraftschurli@gmail.com>
2021-09-13 10:22:24 +0200

    add patchouli_datagen project and patchouli

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/ArsMagicaAPIImpl.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/ArsMagicaAPIImpl.java
@@ -2,11 +2,11 @@ package com.github.minecraftschurli.arsmagicalegacy;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurli.arsmagicalegacy.common.item.Items;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraftforge.fml.ModList;
-//import vazkii.patchouli.api.PatchouliAPI;
+import net.minecraftforge.fml.ModList;
+import vazkii.patchouli.api.PatchouliAPI;
 
 /**
  * @author Minecraftschurli
@@ -21,9 +21,9 @@ public final class ArsMagicaAPIImpl implements ArsMagicaAPI.IArsMagicaAPI {
 
     @Override
     public ItemStack getBookStack() {
-        return //ModList.get().isLoaded("patchouli")
-                //? PatchouliAPI.get().getBookStack(new ResourceLocation(Constants.MOD_ID, "arcane_compendium"))
-                //: // patchouli is not on 1.17.1 yet
-        ItemStack.EMPTY;
+        if (ModList.get().isLoaded("patchouli")) {
+            return PatchouliAPI.get().getBookStack(new ResourceLocation(ArsMagicaAPI.MOD_ID, "arcane_compendium"));
+        }
+        return ItemStack.EMPTY;
     }
 }
--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/compat/patchouli/PatchouliCompat.java
+++
@@ -1,8 +0,0 @@
-package com.github.minecraftschurli.arsmagicalegacy.compat.patchouli;
-
-/**
- * @author Minecraftschurli
- * @version 2021-06-15
- */
-public class PatchouliCompat {
-}
