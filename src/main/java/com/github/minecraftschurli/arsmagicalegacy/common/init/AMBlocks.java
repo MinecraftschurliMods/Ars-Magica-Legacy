d3467c9d4ace024e19d740fa97f655f0ab0229aa
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-09-20 14:35:57 +0200

    add topaz and vinteum

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMBlocks.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMBlocks.java
@@ -15,6 +15,12 @@ public interface AMBlocks {
     RegistryObject<Block> CHIMERITE_ORE = BLOCKS.register("chimerite_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3F, 3F)));
     RegistryObject<Block> DEEPSLATE_CHIMERITE_ORE = BLOCKS.register("deepslate_chimerite_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().color(MaterialColor.DEEPSLATE).strength(4.5F, 3.0F).sound(SoundType.DEEPSLATE)));
     RegistryObject<Block> CHIMERITE_BLOCK = BLOCKS.register("chimerite_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3F, 3F)));
+    RegistryObject<Block> TOPAZ_ORE = BLOCKS.register("topaz_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3F, 3F)));
+    RegistryObject<Block> DEEPSLATE_TOPAZ_ORE = BLOCKS.register("deepslate_topaz_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().color(MaterialColor.DEEPSLATE).strength(4.5F, 3.0F).sound(SoundType.DEEPSLATE)));
+    RegistryObject<Block> TOPAZ_BLOCK = BLOCKS.register("topaz_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3F, 3F)));
+    RegistryObject<Block> VINTEUM_ORE = BLOCKS.register("vinteum_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3F, 3F)));
+    RegistryObject<Block> DEEPSLATE_VINTEUM_ORE = BLOCKS.register("deepslate_vinteum_ore", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().color(MaterialColor.DEEPSLATE).strength(4.5F, 3.0F).sound(SoundType.DEEPSLATE)));
+    RegistryObject<Block> VINTEUM_BLOCK = BLOCKS.register("vinteum_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3F, 3F)));
 
     /**
      * Empty method that is required for classloading
--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMItems.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMItems.java
@@ -28,6 +28,14 @@ public interface AMItems {
     RegistryObject<Item> DEEPSLATE_CHIMERITE_ORE = ITEMS.register("deepslate_chimerite_ore", () -> new BlockItem(AMBlocks.DEEPSLATE_CHIMERITE_ORE.get(), ITEM_64));
     RegistryObject<Item> CHIMERITE = ITEMS.register("chimerite", () -> new Item(ITEM_64));
     RegistryObject<Item> CHIMERITE_BLOCK = ITEMS.register("chimerite_block", () -> new BlockItem(AMBlocks.CHIMERITE_BLOCK.get(), ITEM_64));
+    RegistryObject<Item> TOPAZ_ORE = ITEMS.register("topaz_ore", () -> new BlockItem(AMBlocks.TOPAZ_ORE.get(), ITEM_64));
+    RegistryObject<Item> DEEPSLATE_TOPAZ_ORE = ITEMS.register("deepslate_topaz_ore", () -> new BlockItem(AMBlocks.DEEPSLATE_TOPAZ_ORE.get(), ITEM_64));
+    RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(ITEM_64));
+    RegistryObject<Item> TOPAZ_BLOCK = ITEMS.register("topaz_block", () -> new BlockItem(AMBlocks.TOPAZ_BLOCK.get(), ITEM_64));
+    RegistryObject<Item> VINTEUM_ORE = ITEMS.register("vinteum_ore", () -> new BlockItem(AMBlocks.VINTEUM_ORE.get(), ITEM_64));
+    RegistryObject<Item> DEEPSLATE_VINTEUM_ORE = ITEMS.register("deepslate_vinteum_ore", () -> new BlockItem(AMBlocks.DEEPSLATE_VINTEUM_ORE.get(), ITEM_64));
+    RegistryObject<Item> VINTEUM_DUST = ITEMS.register("vinteum_dust", () -> new Item(ITEM_64));
+    RegistryObject<Item> VINTEUM_BLOCK = ITEMS.register("vinteum_block", () -> new BlockItem(AMBlocks.VINTEUM_BLOCK.get(), ITEM_64));
 
     /**
      * Empty method that is required for classloading
