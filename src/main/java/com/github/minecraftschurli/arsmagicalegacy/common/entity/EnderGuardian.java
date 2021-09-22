8f760c8299c6cb296c57c24677a9caca7af20ed0
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-22 10:50:29 +0200

    Create Entities

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/EnderGuardian.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/EnderGuardian.java
@@ -1,63 +1,17 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.BossEvent;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Pose;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 
-public class EnderGuardian extends AbstractBoss {
-    public EnderGuardian(EntityType<? extends EnderGuardian> type, Level level) {
-        super(type, level, BossEvent.BossBarColor.RED);
-    }
-
-    public static AttributeSupplier.Builder createAttributes() {
-        return Monster.createMonsterAttributes().add(Attributes.FOLLOW_RANGE, Attributes.FOLLOW_RANGE.getDefaultValue()).add(Attributes.MAX_HEALTH, 490D).add(Attributes.ARMOR, 16);
-    }
-
-    @Override
-    public float getEyeHeight(@NotNull Pose pPose) {
-        return 2.5F;
+public class EnderGuardian extends Monster {
+    public EnderGuardian(EntityType<? extends Monster> type, Level level) {
+        super(type, level);
     }
 
     @Override
     protected void registerGoals() {
-        super.registerGoals();
-    }
-
-    @Override
-    protected SoundEvent getAmbientSound() {
-        return null;
-    }
-
-    @Override
-    protected SoundEvent getHurtSound(@NotNull DamageSource pDamageSource) {
-        return null;
-    }
-
-    @Override
-    protected SoundEvent getDeathSound() {
-        return null;
-    }
-
-    @Override
-    protected SoundEvent getAttackSound() {
-        return null;
-    }
-
-    @Override
-    public void aiStep() {
-        super.aiStep();
-    }
-
-    @Override
-    public boolean hurt(@NotNull DamageSource pSource, float pAmount) {
-        return super.hurt(pSource, pAmount);
+        goalSelector.addGoal(1, new RandomLookAroundGoal(this));
     }
 }
