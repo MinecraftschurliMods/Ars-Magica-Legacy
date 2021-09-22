0b11dfd1ac3982a8e9af15d52c13d574992f5820
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-22 10:50:29 +0200

    Create Entities

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ManaCreeper.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ManaCreeper.java
@@ -1,17 +1,28 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
-public class ManaCreeper extends Creeper {
+public class ManaCreeper extends Monster {
     public ManaCreeper(EntityType<? extends Monster> type, Level level) {
-        super((EntityType<? extends Creeper>) type, level);
+        super(type, level);
     }
 
     @Override
-    protected void explodeCreeper() {
-        super.explodeCreeper();
+    protected void registerGoals() {
+        this.goalSelector.addGoal(1, new FloatGoal(this));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 0.8D));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
     }
 }
--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMEntities.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMEntities.java
@@ -4,26 +4,20 @@ import com.github.minecraftschurli.arsmagicalegacy.common.entity.*;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.fmllegacy.RegistryObject;
-import org.jetbrains.annotations.ApiStatus.Internal;
 
-import static com.github.minecraftschurli.arsmagicalegacy.common.init.AMRegistries.ENTITIES;
-
-public interface AMEntities {
-    RegistryObject<EntityType<WaterGuardian>> WATER_GUARDIAN = ENTITIES.register("water_guardian", () -> EntityType.Builder.of(WaterGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(1, 2).build("water_guardian"));
-    RegistryObject<EntityType<FireGuardian>> FIRE_GUARDIAN = ENTITIES.register("fire_guardian", () -> EntityType.Builder.of(FireGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("fire_guardian"));
-    RegistryObject<EntityType<EarthGuardian>> EARTH_GUARDIAN = ENTITIES.register("earth_guardian", () -> EntityType.Builder.of(EarthGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("earth_guardian"));
-    RegistryObject<EntityType<AirGuardian>> AIR_GUARDIAN = ENTITIES.register("air_guardian", () -> EntityType.Builder.of(AirGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("air_guardian"));
-    RegistryObject<EntityType<WinterGuardian>> WINTER_GUARDIAN = ENTITIES.register("winter_guardian", () -> EntityType.Builder.of(WinterGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("winter_guardian"));
-    RegistryObject<EntityType<LightningGuardian>> LIGHTNING_GUARDIAN = ENTITIES.register("lightning_guardian", () -> EntityType.Builder.of(LightningGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("lightning_guardian"));
-    RegistryObject<EntityType<NatureGuardian>> NATURE_GUARDIAN = ENTITIES.register("nature_guardian", () -> EntityType.Builder.of(NatureGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("nature_guardian"));
-    RegistryObject<EntityType<LifeGuardian>> LIFE_GUARDIAN = ENTITIES.register("life_guardian", () -> EntityType.Builder.of(LifeGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("life_guardian"));
-    RegistryObject<EntityType<ArcaneGuardian>> ARCANE_GUARDIAN = ENTITIES.register("arcane_guardian", () -> EntityType.Builder.of(ArcaneGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("arcane_guardian"));
-    RegistryObject<EntityType<EnderGuardian>> ENDER_GUARDIAN = ENTITIES.register("ender_guardian", () -> EntityType.Builder.of(EnderGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("ender_guardian"));
-    RegistryObject<EntityType<Mage>> MAGE = ENTITIES.register("mage", () -> EntityType.Builder.of(Mage::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("mage"));
-    RegistryObject<EntityType<ManaCreeper>> MANA_CREEPER = ENTITIES.register("mana_creeper", () -> EntityType.Builder.of(ManaCreeper::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("mana_creeper"));
-    RegistryObject<EntityType<Dryad>> DRYAD = ENTITIES.register("dryad", () -> EntityType.Builder.of(Dryad::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("dryad"));
-
-    @Internal
-    static void init() {
-    }
+public interface AMEntities extends AMRegistries {
+    public static final RegistryObject<EntityType<?>> FIRE_GUARDIAN = ENTITIES.register("fire_guardian", () -> EntityType.Builder.of(AirGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("fire_guardian"));
+    public static final RegistryObject<EntityType<?>> WATER_GUARDIAN = ENTITIES.register("water_guardian", () -> EntityType.Builder.of(WaterGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("water_guardian"));
+    public static final RegistryObject<EntityType<?>> EARTH_GUARDIAN = ENTITIES.register("earth_guardian", () -> EntityType.Builder.of(EarthGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("earth_guardian"));
+    public static final RegistryObject<EntityType<?>> AIR_GUARDIAN = ENTITIES.register("air_guardian", () -> EntityType.Builder.of(AirGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("air_guardian"));
+    public static final RegistryObject<EntityType<?>> WINTER_GUARDIAN = ENTITIES.register("winter_guardian", () -> EntityType.Builder.of(WinterGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("winter_guardian"));
+    public static final RegistryObject<EntityType<?>> LIGHTNING_GUARDIAN = ENTITIES.register("lightning_guardian", () -> EntityType.Builder.of(LightningGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("lightning_guardian"));
+    public static final RegistryObject<EntityType<?>> NATRURE_GUARDIAN = ENTITIES.register("nature_guardian", () -> EntityType.Builder.of(NatureGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("nature_guardian"));
+    public static final RegistryObject<EntityType<?>> LIFE_GUARDIAN = ENTITIES.register("life_guardian", () -> EntityType.Builder.of(LifeGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("life_guardian"));
+    public static final RegistryObject<EntityType<?>> ARCANE_GUARDIAN = ENTITIES.register("arcane_guardian", () -> EntityType.Builder.of(ArcaneGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("arcane_guardian"));
+    public static final RegistryObject<EntityType<?>> ENDER_GUARDIAN = ENTITIES.register("ender_guardian", () -> EntityType.Builder.of(EnderGuardian::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("ender_guardian"));
+    public static final RegistryObject<EntityType<?>> MAGE = ENTITIES.register("mage", () -> EntityType.Builder.of(Mage::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("mage"));
+    public static final RegistryObject<EntityType<?>> MANA_CREEPER = ENTITIES.register("mana_creeper", () -> EntityType.Builder.of(ManaCreeper::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("mana_creeper"));
+    public static final RegistryObject<EntityType<?>> DRYAD = ENTITIES.register("dryad", () -> EntityType.Builder.of(Dryad::new, MobCategory.MONSTER).clientTrackingRange(8).sized(5, 2).build("dryad"));
+    public static void init(){}
 }
