a02273d0d2330cadb7745569b0df0e2786b269dc
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-22 10:50:29 +0200

    Create Entities

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/WaterGuardian.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/WaterGuardian.java
@@ -1,91 +1,17 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.tags.FluidTags;
-import net.minecraft.world.BossEvent;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.RandomSwimmingGoal;
-import net.minecraft.world.entity.ai.goal.TryFindWaterGoal;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.pathfinder.BlockPathTypes;
-import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
 
-public class WaterGuardian extends AbstractBoss {
-    public WaterGuardian(EntityType<? extends WaterGuardian> type, Level level) {
-        super(type, level, BossEvent.BossBarColor.BLUE);
-        this.setPathfindingMalus(BlockPathTypes.WATER, 0.0F);
-    }
-
-    public static AttributeSupplier.Builder createAttributes() {
-        return Monster.createMonsterAttributes().add(Attributes.FOLLOW_RANGE, Attributes.FOLLOW_RANGE.getDefaultValue()).add(Attributes.MAX_HEALTH, 75D).add(Attributes.ARMOR, 10);
+public class WaterGuardian extends Monster {
+    public WaterGuardian(EntityType<? extends Monster> type, Level level) {
+        super(type, level);
     }
 
     @Override
     protected void registerGoals() {
-        super.registerGoals();
-    }
-
-    @Override
-    protected SoundEvent getAmbientSound() {
-        return null;
-    }
-
-    @Override
-    protected SoundEvent getHurtSound(@NotNull DamageSource pDamageSource) {
-        return null;
-    }
-
-    @Override
-    protected SoundEvent getDeathSound() {
-        return null;
-    }
-
-    @Override
-    protected SoundEvent getAttackSound() {
-        return null;
-    }
-
-    @Override
-    public void aiStep() {
-        super.aiStep();
-    }
-
-    @Override
-    public boolean hurt(@NotNull DamageSource pSource, float pAmount) {
-        return super.hurt(pSource, pAmount);
-    }
-
-    @Override
-    public boolean canBreatheUnderwater() {
-        return true;
-    }
-
-    @Override
-    public float getWalkTargetValue(@NotNull BlockPos pPos, LevelReader pLevel) {
-        return pLevel.getFluidState(pPos).is(FluidTags.WATER) ? 10.0F + pLevel.getBrightness(pPos) - 0.5F : super.getWalkTargetValue(pPos, pLevel);
-    }
-
-    @Override
-    public void travel(@NotNull Vec3 pTravelVector) {
-        if (this.isEffectiveAi() && this.isInWater()) {
-            this.moveRelative(0.1F, pTravelVector);
-            this.move(MoverType.SELF, this.getDeltaMovement());
-            this.setDeltaMovement(this.getDeltaMovement().scale(0.9D));
-        } else {
-            super.travel(pTravelVector);
-        }
+        goalSelector.addGoal(1, new RandomLookAroundGoal(this));
     }
 }
