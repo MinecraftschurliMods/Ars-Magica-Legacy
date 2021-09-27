73395dd2f8342d590f2112e1b93bffccd29af44f
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-27 15:45:29 +0200

    Rendering Water Guardian

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
@@ -1,49 +1,23 @@
 package com.github.minecraftschurli.arsmagicalegacy.client;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
-import com.github.minecraftschurli.arsmagicalegacy.client.gui.RuneBagScreen;
 import com.github.minecraftschurli.arsmagicalegacy.client.model.WaterGuardianModel;
-import com.github.minecraftschurli.arsmagicalegacy.common.init.AMBlocks;
-import com.github.minecraftschurli.arsmagicalegacy.common.init.AMContainers;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import com.github.minecraftschurli.arsmagicalegacy.client.render.WaterGuardianRenderer;
-import com.github.minecraftschurli.arsmagicalegacy.common.init.AMEntities;
-import net.minecraft.client.renderer.entity.CreeperRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD, modid = ArsMagicaAPI.MOD_ID)
-public final class ClientInit {
+public class ClientInit {
     @SubscribeEvent
     static void clientSetup(final FMLClientSetupEvent event) {
-        MenuScreens.register(AMContainers.RUNE_BAG.get(), RuneBagScreen::new);
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.WITCHWOOD_SAPLING.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.WITCHWOOD_DOOR.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.WITCHWOOD_TRAPDOOR.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.AUM.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.CERUBLOSSOM.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.DESERT_NOVA.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.TARMA_ROOT.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.WAKEBLOOM.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.VINTEUM_TORCH.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.VINTEUM_WALL_TORCH.get(), RenderType.cutout());
-        ItemBlockRenderTypes.setRenderLayer(AMBlocks.WIZARDS_CHALK.get(), RenderType.cutout());
     }
 
     @SubscribeEvent
     static void registerLayerDefinitions(final EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(WaterGuardianModel.LAYER_LOCATION, WaterGuardianModel::createBodyLayer);
-    }
-
-    @SubscribeEvent
-    static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
-        event.registerEntityRenderer(AMEntities.WATER_GUARDIAN.get(), WaterGuardianRenderer::new);
-        event.registerEntityRenderer(AMEntities.MANA_CREEPER.get(), CreeperRenderer::new);
     }
 
 //    @SubscribeEvent
