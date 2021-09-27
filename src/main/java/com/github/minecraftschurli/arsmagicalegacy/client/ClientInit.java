d2f7fcd95cf43a057a35557ec026ab1eae852759
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-27 15:45:29 +0200

    Rendering Water Guardian

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
@@ -2,12 +2,14 @@ package com.github.minecraftschurli.arsmagicalegacy.client;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurli.arsmagicalegacy.client.gui.RuneBagScreen;
+import com.github.minecraftschurli.arsmagicalegacy.client.model.WaterGuardianModel;
 import com.github.minecraftschurli.arsmagicalegacy.common.init.AMBlocks;
 import com.github.minecraftschurli.arsmagicalegacy.common.init.AMContainers;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -29,4 +31,14 @@ public final class ClientInit {
         ItemBlockRenderTypes.setRenderLayer(AMBlocks.VINTEUM_WALL_TORCH.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(AMBlocks.WIZARDS_CHALK.get(), RenderType.cutout());
     }
+
+    @SubscribeEvent
+    static void registerLayerDefinitions(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(WaterGuardianModel.LAYER_LOCATION, WaterGuardianModel::createBodyLayer);
+    }
+
+//    @SubscribeEvent
+//    public static void onRegisterEntities(final RegistryEvent.Register<EntityType<?>> event) {
+//        ModSpawnEggItem.initSpawnEggs();
+//    }
 }
