ab2867449fb51972ee2ad8cf4811f16270713f5d
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-27 15:45:29 +0200

    Rendering Water Guardian

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
@@ -1,20 +1,15 @@
 package com.github.minecraftschurli.arsmagicalegacy.client;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
-import com.github.minecraftschurli.arsmagicalegacy.api.affinity.IAffinity;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.InventoryMenu;
 import com.github.minecraftschurli.arsmagicalegacy.client.gui.RuneBagScreen;
+import com.github.minecraftschurli.arsmagicalegacy.client.model.WaterGuardianModel;
 import com.github.minecraftschurli.arsmagicalegacy.common.init.AMBlocks;
 import com.github.minecraftschurli.arsmagicalegacy.common.init.AMContainers;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
-import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -22,7 +17,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD, modid = ArsMagicaAPI.MOD_ID)
 public final class ClientInit {
     @SubscribeEvent
-    static void clientSetup(FMLClientSetupEvent event) {
+    static void clientSetup(final FMLClientSetupEvent event) {
         MenuScreens.register(AMContainers.RUNE_BAG.get(), RuneBagScreen::new);
         ItemBlockRenderTypes.setRenderLayer(AMBlocks.WITCHWOOD_SAPLING.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(AMBlocks.WITCHWOOD_DOOR.get(), RenderType.cutout());
@@ -38,28 +33,12 @@ public final class ClientInit {
     }
 
     @SubscribeEvent
-    static void preStitch(TextureStitchEvent.Pre event) {
-        if (!event.getMap().location().equals(InventoryMenu.BLOCK_ATLAS)) return;
-        //event.addSprite(CraftingAltarModel.OVERLAY_LOC);
+    static void registerLayerDefinitions(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(WaterGuardianModel.LAYER_LOCATION, WaterGuardianModel::createBodyLayer);
     }
 
-    @SubscribeEvent
-    static void postStitch(TextureStitchEvent.Post event) {
-        if (!event.getMap().location().equals(InventoryMenu.BLOCK_ATLAS)) return;
-        //CraftingAltarModel.OVERLAY = event.getMap().getSprite(CraftingAltarModel.OVERLAY_LOC);
-    }
-
-    @SubscribeEvent
-    static void modelRegister(ModelRegistryEvent event) {
-        for (IAffinity affinity : ArsMagicaAPI.get().getAffinityRegistry()) {
-            if (IAffinity.NONE.equals(affinity.getRegistryName())) continue;
-            ModelLoader.addSpecialModel(new ResourceLocation(affinity.getId().getNamespace(), "affinity_essence_"+affinity.getId().getPath()));
-            ModelLoader.addSpecialModel(new ResourceLocation(affinity.getId().getNamespace(), "affinity_tome_"+affinity.getId().getPath()));
-        }
-    }
-
-    @SubscribeEvent
-    static void modelBake(ModelBakeEvent event) {
-
-    }
+//    @SubscribeEvent
+//    public static void onRegisterEntities(final RegistryEvent.Register<EntityType<?>> event) {
+//        ModSpawnEggItem.initSpawnEggs();
+//    }
 }
