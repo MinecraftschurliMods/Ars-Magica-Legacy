cd932ea59c9e4593c267ea1f6a9b3fa8202cab49
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-09-28 08:49:39 +0200

    make the water guardian render

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/ClientInit.java
@@ -2,6 +2,8 @@ package com.github.minecraftschurli.arsmagicalegacy.client;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurli.arsmagicalegacy.client.model.WaterGuardianModel;
+import com.github.minecraftschurli.arsmagicalegacy.client.render.WaterGuardianRenderer;
+import com.github.minecraftschurli.arsmagicalegacy.common.init.AMEntities;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,6 +20,11 @@ public class ClientInit {
     @SubscribeEvent
     static void registerLayerDefinitions(final EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(WaterGuardianModel.LAYER_LOCATION, WaterGuardianModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(AMEntities.WATER_GUARDIAN.get(), WaterGuardianRenderer::new);
     }
 
 //    @SubscribeEvent
