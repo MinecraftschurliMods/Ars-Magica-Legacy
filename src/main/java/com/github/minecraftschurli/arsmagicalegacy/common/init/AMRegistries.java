1ee6d6fb063442c3de42887271e89721d3305bcb
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-28 13:25:53 +0200

    Rendering Water Guardian minor fixes

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMRegistries.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMRegistries.java
@@ -53,6 +53,7 @@ public interface AMRegistries {
         BLOCK_ENTITIES.register(bus);
         PARTICLE_TYPES.register(bus);
         CONTAINERS.register(bus);
+
         RECIPE_SERIALIZERS.register(bus);
         STAT_TYPES.register(bus);
     }
