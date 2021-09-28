17f8471613fd2ca4e781c812e21d3415523e551e
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-09-28 13:25:53 +0200

    Rendering Water Guardian minor fixes

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/render/WaterGuardianRenderer.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/render/WaterGuardianRenderer.java
@@ -6,6 +6,7 @@ import com.github.minecraftschurli.arsmagicalegacy.common.entity.WaterGuardian;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fluids.FluidAttributes;
 import org.jetbrains.annotations.NotNull;
 
 public class WaterGuardianRenderer extends MobRenderer<WaterGuardian, WaterGuardianModel<WaterGuardian>> {
@@ -16,7 +17,8 @@ public class WaterGuardianRenderer extends MobRenderer<WaterGuardian, WaterGuard
     }
 
     @Override
-    public @NotNull ResourceLocation getTextureLocation(final WaterGuardian pEntity) {
+    public ResourceLocation getTextureLocation(final WaterGuardian pEntity) {
         return TEXTURE;
     }
+
 }
