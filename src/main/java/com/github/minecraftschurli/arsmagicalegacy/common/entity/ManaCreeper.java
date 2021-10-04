466ab34a8a9226a6696640679f540218b338866e
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-10-04 14:49:22 +0200

    Water Guardian behavior and added Mana Creeper

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ManaCreeper.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ManaCreeper.java
@@ -1,28 +1,17 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
-import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
-import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
-public class ManaCreeper extends Monster {
+public class ManaCreeper extends Creeper {
     public ManaCreeper(EntityType<? extends Monster> type, Level level) {
-        super(type, level);
+        super((EntityType<? extends Creeper>) type, level);
     }
 
     @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 0.8D));
-        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
-        this.goalSelector.addGoal(6, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+    protected void explodeCreeper() {
+        super.explodeCreeper();
     }
 }
