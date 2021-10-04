423c2b257e00b05f41ab0b02999e97029f4486fa
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-10-04 14:49:22 +0200

    Water Guardian behavior and added Mana Creeper

---
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ai/ExecuteSpellGoal.java
@@ -0,0 +1,11 @@
+package com.github.minecraftschurli.arsmagicalegacy.common.entity.ai;
+
+import com.github.minecraftschurli.arsmagicalegacy.common.entity.WaterGuardian;
+
+public class ExecuteSpellGoal {
+    private WaterGuardian guardian;
+
+    public ExecuteSpellGoal(WaterGuardian guardian) {
+        this.guardian = guardian;
+    }
+}
