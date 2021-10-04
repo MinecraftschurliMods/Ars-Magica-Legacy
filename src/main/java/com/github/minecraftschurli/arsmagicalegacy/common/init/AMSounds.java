bec3d67eebc34f9d56ad1455ad569c8cd7be83cb
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-10-05 00:09:09 +0200

    sounds work!

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMSounds.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMSounds.java
@@ -1,11 +1,24 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.init;
 
+import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import org.jetbrains.annotations.ApiStatus.NonExtendable;
+
+import static com.github.minecraftschurli.arsmagicalegacy.common.init.AMRegistries.SOUND_EVENTS;
 
 @NonExtendable
 public interface AMSounds {
-
+    RegistryObject<SoundEvent> WATER_GUARDIAN_AMBIENT = register("entity.water_guardian.ambient");
+    RegistryObject<SoundEvent> WATER_GUARDIAN_ATTACK = register("entity.water_guardian.attack");
+    RegistryObject<SoundEvent> WATER_GUARDIAN_DEATH = register("entity.water_guardian.death");
+    RegistryObject<SoundEvent> WATER_GUARDIAN_HURT = register("entity.water_guardian.hurt");
 
     static void register() {
+    }
+
+    private static RegistryObject<SoundEvent> register(String id) {
+        return SOUND_EVENTS.register(id, () -> new SoundEvent(new ResourceLocation(ArsMagicaAPI.MOD_ID, id)));
     }
 }
