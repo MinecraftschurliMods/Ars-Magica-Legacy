92e959df15bd69949b091b37df0daf087ff34180
asinghTGM <56579311+asinghTGM@users.noreply.github.com>
2021-10-04 14:49:22 +0200

    Water Guardian behavior and added Mana Creeper

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/render/WaterGuardianRenderer.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/client/render/WaterGuardianRenderer.java
@@ -6,8 +6,6 @@ import com.github.minecraftschurli.arsmagicalegacy.common.entity.WaterGuardian;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fluids.FluidAttributes;
-import org.jetbrains.annotations.NotNull;
 
 public class WaterGuardianRenderer extends MobRenderer<WaterGuardian, WaterGuardianModel<WaterGuardian>> {
 
