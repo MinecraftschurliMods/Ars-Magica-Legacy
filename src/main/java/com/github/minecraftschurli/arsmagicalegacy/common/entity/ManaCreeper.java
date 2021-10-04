c8243acd85059c1980b0baff8489ac011ef3f77f
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-10-04 15:50:34 +0200

    fix mana creeper not spawning

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ManaCreeper.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/ManaCreeper.java
@@ -1,13 +1,19 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
 
 public class ManaCreeper extends Creeper {
-    public ManaCreeper(EntityType<? extends Monster> type, Level level) {
-        super((EntityType<? extends Creeper>) type, level);
+    public ManaCreeper(EntityType<? extends ManaCreeper> type, Level level) {
+        super(type, level);
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Monster.createMonsterAttributes().add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 
     @Override
