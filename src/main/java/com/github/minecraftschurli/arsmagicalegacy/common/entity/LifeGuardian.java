40cd31e4075c0069441fba77e6fdff5d486e36ff
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-10-04 21:30:53 +0200

    create entity attributes, create AbstractBoss, many small improvements

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/LifeGuardian.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/LifeGuardian.java
@@ -1,17 +1,63 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.BossEvent;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Pose;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
-public class LifeGuardian extends Monster {
-    public LifeGuardian(EntityType<? extends Monster> type, Level level) {
-        super(type, level);
+public class LifeGuardian extends AbstractBoss {
+    public LifeGuardian(EntityType<? extends LifeGuardian> type, Level level) {
+        super(type, level, BossEvent.BossBarColor.GREEN);
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Monster.createMonsterAttributes().add(Attributes.FOLLOW_RANGE, Attributes.FOLLOW_RANGE.getDefaultValue()).add(Attributes.MAX_HEALTH, 200D).add(Attributes.ARMOR, Attributes.ARMOR.getDefaultValue());
+    }
+
+    @Override
+    public float getEyeHeight(@NotNull Pose pPose) {
+        return 1.5F;
     }
 
     @Override
     protected void registerGoals() {
-        goalSelector.addGoal(1, new RandomLookAroundGoal(this));
+        super.registerGoals();
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return null;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(@NotNull DamageSource pDamageSource) {
+        return null;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return null;
+    }
+
+    @Override
+    protected SoundEvent getAttackSound() {
+        return null;
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+    }
+
+    @Override
+    public boolean hurt(@NotNull DamageSource pSource, float pAmount) {
+        return super.hurt(pSource, pAmount);
     }
 }
