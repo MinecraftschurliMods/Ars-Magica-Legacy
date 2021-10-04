8974182f361231d3ffdf4b89fb8b6af508388856
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-10-04 10:07:57 +0200

    add book categories

--- src/data/java/com/github/minecraftschurli/arsmagicalegacy/data/AMPatchouliBookProvider.java
+++ src/data/java/com/github/minecraftschurli/arsmagicalegacy/data/AMPatchouliBookProvider.java
@@ -1,6 +1,7 @@
 package com.github.minecraftschurli.arsmagicalegacy.data;
 
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
+import com.github.minecraftschurli.arsmagicalegacy.common.init.AMItems;
 import com.github.minecraftschurli.patchouli_datagen.BookBuilder;
 import com.github.minecraftschurli.patchouli_datagen.PatchouliBookProvider;
 import net.minecraft.data.DataGenerator;
@@ -23,6 +24,22 @@ class AMPatchouliBookProvider extends PatchouliBookProvider {
     protected void addBooks(Consumer<BookBuilder<?, ?, ?>> consumer) {
         createBookBuilder("arcane_compendium", "Arcane Compendium", "A renewed look into Minecraft with a splash of magic...", lang).setSubtitle("1st Edition").setModel(new ResourceLocation(ArsMagicaAPI.MOD_ID, "arcane_compendium")).setCreativeTab(ArsMagicaAPI.get().getItemGroup().getRecipeFolderName()).setUseResourcepack()
 //                .addCategory("test", "TestCategory", "This is a test", new ItemStack(Items.BARRIER)).addEntry("test", "TestEntry", new ItemStack(Items.GLOW_BERRIES)).addSimpleTextPage("Lorem ipsum dolor sit").build().build()
+                .addCategory("mechanics", "Mechanics", "", new ItemStack(AMItems.BLANK_RUNE.get())) //TODO: magitech goggles here instead
+                .setSortnum(0).build()
+                .addCategory("blocks", "Blocks", "", new ItemStack(AMItems.CHIMERITE_ORE.get())) //TODO: crystal wrench here instead
+                .setSortnum(1).build()
+                .addCategory("items", "Items", "", new ItemStack(AMItems.VINTEUM_DUST.get()))
+                .setSortnum(2).build()
+                .addCategory("entities", "Entities", "", new ItemStack(AMItems.PURIFIED_VINTEUM_DUST.get()))
+                .setSortnum(3).build()
+                .addCategory("shapes", "Shapes", "", "") //TODO: add icon
+                .setSortnum(4).build()
+                .addCategory("components", "Components", "", "") //TODO: add icon
+                .setSortnum(5).build()
+                .addCategory("modifiers", "Modifiers", "", "") //TODO: add icon
+                .setSortnum(6).build()
+                .addCategory("talents", "Talents", "", "") //TODO: add icon
+                .setSortnum(7).build()
                 .build(consumer);
     }
 }
