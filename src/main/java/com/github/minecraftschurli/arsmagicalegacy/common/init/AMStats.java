b616d1d7509a5bd32632ed270461a5d5d2f42593
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-11-09 08:45:32 +0100

    add effect registry, rearrange init classes

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMStats.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMStats.java
@@ -16,9 +16,9 @@ import java.util.Map;
 public interface AMStats {
     Map<ResourceLocation, StatFormatter> STAT_REGISTER = new HashMap<>();
 
-    ResourceLocation INTERACT_WITH_OCCULUS           = register(new ResourceLocation(ArsMagicaAPI.MOD_ID, "interact_with_occulus"), StatFormatter.DEFAULT);
+    ResourceLocation INTERACT_WITH_OCCULUS           = register(new ResourceLocation(ArsMagicaAPI.MOD_ID, "interact_with_occulus"),           StatFormatter.DEFAULT);
     ResourceLocation INTERACT_WITH_INSCRIPTION_TABLE = register(new ResourceLocation(ArsMagicaAPI.MOD_ID, "interact_with_inscription_table"), StatFormatter.DEFAULT);
-    ResourceLocation SPELL_CAST                      = register(new ResourceLocation(ArsMagicaAPI.MOD_ID, "spell_cast"), StatFormatter.DEFAULT);
+    ResourceLocation SPELL_CAST                      = register(new ResourceLocation(ArsMagicaAPI.MOD_ID, "spell_cast"),                      StatFormatter.DEFAULT);
 
     @SuppressWarnings("SameParameterValue")
     private static ResourceLocation register(ResourceLocation location, StatFormatter formatter) {
@@ -34,6 +34,9 @@ public interface AMStats {
         });
     }
 
+    /**
+     * Empty method that is required for classloading
+     */
     @Internal
     static void register() {}
 }
