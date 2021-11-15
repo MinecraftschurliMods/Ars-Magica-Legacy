ce3fa75bfaae372261f5866ec9a0b736a8532418
Minecraftschurli <minecraftschurli@gmail.com>
2021-11-15 15:44:56 +0100

    fix touch shape

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/spell/shape/Touch.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/spell/shape/Touch.java
@@ -5,7 +5,6 @@ import com.github.minecraftschurli.arsmagicalegacy.api.spell.ISpell;
 import com.github.minecraftschurli.arsmagicalegacy.api.spell.ISpellModifier;
 import com.github.minecraftschurli.arsmagicalegacy.api.spell.SpellCastResult;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.HitResult;
 
@@ -14,6 +13,6 @@ import java.util.List;
 public class Touch extends AbstractShape {
     @Override
     public SpellCastResult invoke(ISpell spell, LivingEntity caster, Level level, List<ISpellModifier> modifiers, HitResult hit, int ticksUsed, int index, boolean awardXp) {
-        return ArsMagicaAPI.get().getSpellHelper().invoke(spell, caster, level, ProjectileUtil.getHitResult(caster, entity -> !entity.isSpectator()), ticksUsed, index, awardXp);
+        return ArsMagicaAPI.get().getSpellHelper().invoke(spell, caster, level, ArsMagicaAPI.get().getSpellHelper().trace(caster, level, 2.5f, true, false), ticksUsed, index, awardXp);
     }
 }
