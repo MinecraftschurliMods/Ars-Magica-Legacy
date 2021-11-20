ecab70851375b5bbedf8cc841f1eacc7f2a452e7
Minecraftschurli <minecraftschurli@gmail.com>
2021-11-20 17:32:49 +0100

    make etherium type translatable

--- src/api/java/com/github/minecraftschurli/arsmagicalegacy/api/etherium/EtheriumType.java
+++ src/api/java/com/github/minecraftschurli/arsmagicalegacy/api/etherium/EtheriumType.java
@@ -1,8 +1,22 @@
 package com.github.minecraftschurli.arsmagicalegacy.api.etherium;
 
+import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
+import com.github.minecraftschurli.arsmagicalegacy.api.util.ITranslatable;
+import net.minecraft.resources.ResourceLocation;
+
 /**
  * The etherium types.
  */
-public enum EtheriumType  {
-    LIGHT, NEUTRAL, DARK
+public enum EtheriumType implements ITranslatable {
+    LIGHT, NEUTRAL, DARK;
+
+    @Override
+    public ResourceLocation getId() {
+        return new ResourceLocation(ArsMagicaAPI.MOD_ID, name().toLowerCase());
+    }
+
+    @Override
+    public String getType() {
+        return "etherium";
+    }
 }
