fe143d4b9d02baea32b47458e2625791e53fb312
Minecraftschurli <minecraftschurli@gmail.com>
2021-11-24 21:01:28 +0100

    fix spell rune

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/block/SpellRuneBlock.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/block/SpellRuneBlock.java
@@ -1,10 +1,8 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.block;
 
-import com.github.minecraftschurli.arsmagicalegacy.common.init.AMMobEffects;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -18,27 +16,25 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.EntityCollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public class SpellRuneBlock extends Block implements EntityBlock {
-    public  static final DirectionProperty FACE = BlockStateProperties.FACING;
+    public static final DirectionProperty FACE = BlockStateProperties.FACING;
 
-    private static final Map<Direction, VoxelShape> COLLISION_SHAPES = Map.of(
+    public static final Map<Direction, VoxelShape> COLLISION_SHAPES = Map.of(
             Direction.DOWN,  Block.box( 1.0D,  0.0D,  1.0D, 15.0D,  0.5D, 15.0D),
             Direction.UP,    Block.box( 1.0D, 15.5D,  1.0D, 15.0D, 16.0D, 15.0D),
-            Direction.NORTH, Block.box( 0.0D,  1.0D,  1.0D,  0.5D, 15.0D, 15.0D),
-            Direction.SOUTH, Block.box(15.5D,  1.0D,  1.0D, 16.0D, 15.0D, 15.0D),
-            Direction.WEST,  Block.box( 1.0D,  1.0D,  0.0D, 15.0D, 15.0D,  0.5D),
-            Direction.EAST,  Block.box( 1.0D,  1.0D, 15.5D, 15.0D, 15.0D, 16.0D)
+            Direction.NORTH, Block.box( 1.0D,  1.0D,  0.0D, 15.0D, 15.0D,  0.5D),
+            Direction.SOUTH, Block.box( 1.0D,  1.0D, 15.5D, 15.0D, 15.0D, 16.0D),
+            Direction.WEST,  Block.box( 0.0D,  1.0D,  1.0D,  0.5D, 15.0D, 15.0D),
+            Direction.EAST,  Block.box(15.5D,  1.0D,  1.0D, 16.0D, 15.0D, 15.0D)
     );
 
     public SpellRuneBlock() {
-        super(BlockBehaviour.Properties.of(Material.AIR).air());
+        super(BlockBehaviour.Properties.of(Material.AIR).noCollission().air());
         registerDefaultState(getStateDefinition().any().setValue(FACE, Direction.DOWN));
     }
 
@@ -55,23 +51,6 @@ public class SpellRuneBlock extends Block implements EntityBlock {
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        if (pContext instanceof EntityCollisionContext entityCollisionContext &&
-            entityCollisionContext.getEntity().isPresent() &&
-            entityCollisionContext.getEntity().get() instanceof Player player &&
-            player.level.isClientSide() &&
-            (player.isCreative() || player.hasEffect(AMMobEffects.TRUE_SIGHT.get()))) {
-            return this.getCollisionShape(pState, pLevel, pPos, pContext);
-        }
-        return Shapes.empty();
-    }
-
-    @Override
-    public VoxelShape getVisualShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return this.getShape(pState, pLevel, pPos, pContext);
-    }
-
-    @Override
-    public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return COLLISION_SHAPES.get(pState.getValue(FACE));
     }
 
