3b02698b47ef069448777cbb0f61007252887ee3
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-11-29 13:45:19 +0100

    add entity data

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/WallEntity.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/entity/WallEntity.java
@@ -1,14 +1,28 @@
 package com.github.minecraftschurli.arsmagicalegacy.common.entity;
 
+import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurli.arsmagicalegacy.common.init.AMEntities;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
 public class WallEntity extends Entity {
+    private static final EntityDataAccessor<Boolean> TARGET_NON_SOLID = SynchedEntityData.defineId(ProjectileEntity.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> DURATION = SynchedEntityData.defineId(ProjectileEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> INDEX = SynchedEntityData.defineId(ProjectileEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> OWNER = SynchedEntityData.defineId(ProjectileEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Float> RADIUS = SynchedEntityData.defineId(ProjectileEntity.class, EntityDataSerializers.FLOAT);
+    private static final EntityDataAccessor<ItemStack> STACK = SynchedEntityData.defineId(ProjectileEntity.class, EntityDataSerializers.ITEM_STACK);
+
     public WallEntity(EntityType<? extends WallEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
@@ -24,21 +38,101 @@ public class WallEntity extends Entity {
 
     @Override
     protected void defineSynchedData() {
-
+        entityData.define(TARGET_NON_SOLID, false);
+        entityData.define(DURATION, 200);
+        entityData.define(INDEX, 0);
+        entityData.define(OWNER, 0);
+        entityData.define(RADIUS, 1.4f);
+        entityData.define(STACK, ItemStack.EMPTY);
     }
 
     @Override
     protected void readAdditionalSaveData(CompoundTag pCompound) {
-
+        CompoundTag tag = pCompound.getCompound(ArsMagicaAPI.MOD_ID);
+        entityData.set(TARGET_NON_SOLID, tag.getBoolean("TargetNonSolid"));
+        entityData.set(DURATION, tag.getInt("Duration"));
+        entityData.set(INDEX, tag.getInt("Index"));
+        entityData.set(OWNER, tag.getInt("Owner"));
+        entityData.set(RADIUS, tag.getFloat("Radius"));
+        entityData.set(STACK, ItemStack.of(tag.getCompound("Stack")));
     }
 
     @Override
     protected void addAdditionalSaveData(CompoundTag pCompound) {
-
+        CompoundTag tag = pCompound.getCompound(ArsMagicaAPI.MOD_ID);
+        tag.putBoolean("TargetNonSolid", entityData.get(TARGET_NON_SOLID));
+        tag.putInt("Duration", entityData.get(DURATION));
+        tag.putInt("Index", entityData.get(INDEX));
+        tag.putInt("Owner", entityData.get(OWNER));
+        tag.putFloat("Radius", entityData.get(RADIUS));
+        CompoundTag stack = new CompoundTag();
+        entityData.get(STACK).save(stack);
+        tag.put("Stack", stack);
     }
 
     @Override
     public Packet<?> getAddEntityPacket() {
-        return new ClientboundAddEntityPacket(this);
+        Entity entity = getOwner();
+        return new ClientboundAddEntityPacket(this, entity == null ? 0 : entity.getId());
+    }
+
+    @Override
+    public void tick() {
+        if (tickCount > getDuration() || getOwner() == null) {
+            remove(RemovalReason.KILLED);
+            return;
+        }
+    }
+
+    public boolean getTargetNonSolid() {
+        return entityData.get(TARGET_NON_SOLID);
+    }
+
+    public void setTargetNonSolid() {
+        entityData.set(TARGET_NON_SOLID, true);
+    }
+
+    public int getDuration() {
+        return entityData.get(DURATION);
+    }
+
+    public void setDuration(int duration) {
+        entityData.set(DURATION, duration);
+    }
+
+    public int getIndex() {
+        return entityData.get(INDEX);
+    }
+
+    public void setIndex(int index) {
+        entityData.set(INDEX, index);
+    }
+
+    @Nullable
+    public LivingEntity getOwner() {
+        Entity entity = level.getEntity(entityData.get(OWNER));
+        return entity instanceof LivingEntity ? (LivingEntity) entity : null;
+    }
+
+    public void setOwner(Entity owner) {
+        if (owner instanceof LivingEntity) {
+            entityData.set(OWNER, owner.getId());
+        }
+    }
+
+    public float getRadius() {
+        return entityData.get(RADIUS);
+    }
+
+    public void setRadius(float radius) {
+        entityData.set(RADIUS, radius);
+    }
+
+    public ItemStack getStack() {
+        return entityData.get(STACK);
+    }
+
+    public void setStack(ItemStack stack) {
+        entityData.set(STACK, stack);
     }
 }
