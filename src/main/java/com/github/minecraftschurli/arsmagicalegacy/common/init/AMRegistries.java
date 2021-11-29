c1f893989303608f085742e27c4c3917e011564a
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-11-29 10:34:07 +0100

    fix a NPE with sounds

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMRegistries.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMRegistries.java
@@ -3,12 +3,14 @@ package com.github.minecraftschurli.arsmagicalegacy.common.init;
 import com.github.minecraftschurli.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurli.arsmagicalegacy.api.affinity.IAffinity;
 import com.github.minecraftschurli.arsmagicalegacy.api.skill.ISkillPoint;
+import com.github.minecraftschurli.arsmagicalegacy.api.spell.ISpellPart;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.stats.StatType;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.alchemy.Potion;
@@ -33,13 +35,14 @@ public interface AMRegistries {
     DeferredRegister<Fluid>               FLUIDS               = DeferredRegister.create(ForgeRegistries.FLUIDS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<Item>                ITEMS                = DeferredRegister.create(ForgeRegistries.ITEMS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<MobEffect>           MOB_EFFECTS          = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, ArsMagicaAPI.MOD_ID);
+    DeferredRegister<Attribute>           ATTRIBUTES           = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, ArsMagicaAPI.MOD_ID);
     DeferredRegister<SoundEvent>          SOUND_EVENTS         = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<Potion>              POTIONS              = DeferredRegister.create(ForgeRegistries.POTIONS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<Enchantment>         ENCHANTMENTS         = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<EntityType<?>>       ENTITIES             = DeferredRegister.create(ForgeRegistries.ENTITIES, ArsMagicaAPI.MOD_ID);
     DeferredRegister<BlockEntityType<?>>  BLOCK_ENTITIES       = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, ArsMagicaAPI.MOD_ID);
     DeferredRegister<ParticleType<?>>     PARTICLE_TYPES       = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, ArsMagicaAPI.MOD_ID);
-    DeferredRegister<MenuType<?>>         CONTAINERS           = DeferredRegister.create(ForgeRegistries.CONTAINERS, ArsMagicaAPI.MOD_ID);
+    DeferredRegister<MenuType<?>>         MENU_TYPES           = DeferredRegister.create(ForgeRegistries.CONTAINERS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS   = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, ArsMagicaAPI.MOD_ID);
     DeferredRegister<StatType<?>>         STAT_TYPES           = DeferredRegister.create(ForgeRegistries.STAT_TYPES, ArsMagicaAPI.MOD_ID);
 
@@ -49,6 +52,8 @@ public interface AMRegistries {
     DeferredRegister<IAffinity>           AFFINITIES           = DeferredRegister.create(IAffinity.class, ArsMagicaAPI.MOD_ID);
     Supplier<IForgeRegistry<IAffinity>>   AFFINITY_REGISTRY    = AFFINITIES.makeRegistry("affinity", () -> new RegistryBuilder<IAffinity>().setDefaultKey(new ResourceLocation(ArsMagicaAPI.MOD_ID, "none")));
 
+    DeferredRegister<ISpellPart>          SPELL_PARTS          = DeferredRegister.create(ISpellPart.class, ArsMagicaAPI.MOD_ID);
+    Supplier<IForgeRegistry<ISpellPart>>  SPELL_PART_REGISTRY  = SPELL_PARTS.makeRegistry("spell_part", RegistryBuilder::new);
 
     /**
      * @param bus the mods event bus
@@ -57,11 +62,17 @@ public interface AMRegistries {
     static void init(IEventBus bus) {
         AMBlocks.register();
         AMItems.register();
+        AMMobEffects.register();
+        AMAttributes.register();
         AMSounds.register();
         AMEntities.register();
-        AMContainers.register();
+        AMBlockEntities.register();
+        AMMenuTypes.register();
+        AMStats.register();
+        bus.addListener(AMStats::onRegister);
         AMSkillPoints.register();
         AMAffinities.register();
+        AMSpellParts.register();
         BLOCKS.register(bus);
         FLUIDS.register(bus);
         ITEMS.register(bus);
@@ -72,11 +83,12 @@ public interface AMRegistries {
         ENTITIES.register(bus);
         BLOCK_ENTITIES.register(bus);
         PARTICLE_TYPES.register(bus);
-        CONTAINERS.register(bus);
-
+        MENU_TYPES.register(bus);
         RECIPE_SERIALIZERS.register(bus);
         STAT_TYPES.register(bus);
+        ATTRIBUTES.register(bus);
         SKILL_POINTS.register(bus);
         AFFINITIES.register(bus);
+        SPELL_PARTS.register(bus);
     }
 }
