7a1d55535fba045bdbeb68b1bba37a24cabb611b
Minecraftschurli <minecraftschurli@gmail.com>
2021-12-02 13:53:28 +0100

    switch to BlockEntityType.Builder

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMBlockEntities.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/init/AMBlockEntities.java
@@ -9,16 +9,14 @@ import net.minecraftforge.fmllegacy.RegistryObject;
 import org.jetbrains.annotations.ApiStatus.Internal;
 import org.jetbrains.annotations.ApiStatus.NonExtendable;
 
-import java.util.Set;
-
 import static com.github.minecraftschurli.arsmagicalegacy.common.init.AMRegistries.BLOCK_ENTITIES;
 
 @NonExtendable
 public interface AMBlockEntities {
-    RegistryObject<BlockEntityType<AltarCoreBlockEntity>>        ALTAR_CORE        = BLOCK_ENTITIES.register("altar_core",        () -> new BlockEntityType<>(AltarCoreBlockEntity::new,        Set.of(AMBlocks.ALTAR_CORE.get()),        null));
-    RegistryObject<BlockEntityType<AltarViewBlockEntity>>        ALTAR_VIEW        = BLOCK_ENTITIES.register("altar_view",        () -> new BlockEntityType<>(AltarViewBlockEntity::new,        Set.of(AMBlocks.ALTAR_VIEW.get()),        null));
-    RegistryObject<BlockEntityType<InscriptionTableBlockEntity>> INSCRIPTION_TABLE = BLOCK_ENTITIES.register("inscription_table", () -> new BlockEntityType<>(InscriptionTableBlockEntity::new, Set.of(AMBlocks.INSCRIPTION_TABLE.get()), null));
-    RegistryObject<BlockEntityType<SpellRuneBlockEntity>>        SPELL_RUNE        = BLOCK_ENTITIES.register("spell_rune",        () -> new BlockEntityType<>(SpellRuneBlockEntity::new,        Set.of(AMBlocks.SPELL_RUNE.get()),        null));
+    RegistryObject<BlockEntityType<AltarCoreBlockEntity>>        ALTAR_CORE        = BLOCK_ENTITIES.register("altar_core",        () -> BlockEntityType.Builder.of(AltarCoreBlockEntity::new,        AMBlocks.ALTAR_CORE.get())       .build(null));
+    RegistryObject<BlockEntityType<AltarViewBlockEntity>>        ALTAR_VIEW        = BLOCK_ENTITIES.register("altar_view",        () -> BlockEntityType.Builder.of(AltarViewBlockEntity::new,        AMBlocks.ALTAR_VIEW.get())       .build(null));
+    RegistryObject<BlockEntityType<InscriptionTableBlockEntity>> INSCRIPTION_TABLE = BLOCK_ENTITIES.register("inscription_table", () -> BlockEntityType.Builder.of(InscriptionTableBlockEntity::new, AMBlocks.INSCRIPTION_TABLE.get()).build(null));
+    RegistryObject<BlockEntityType<SpellRuneBlockEntity>>        SPELL_RUNE        = BLOCK_ENTITIES.register("spell_rune",        () -> BlockEntityType.Builder.of(SpellRuneBlockEntity::new,        AMBlocks.SPELL_RUNE.get())       .build(null));
 
     /**
      * Empty method that is required for classloading
