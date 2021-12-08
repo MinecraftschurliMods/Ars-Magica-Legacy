e8659ebb20f5dda52461aefb0380ee1fe128257b
Minecraftschurli <minecraftschurli@gmail.com>
2021-12-08 22:44:43 +0100

    add missing package-info.java update curios and forge and fix namespace for oregen

---
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/level/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.github.minecraftschurli.arsmagicalegacy.common.level;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
