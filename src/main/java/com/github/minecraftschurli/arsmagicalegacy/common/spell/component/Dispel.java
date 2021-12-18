3d9f71a26aea86ba269995d870302f4c9e4b71f5
IchHabeHunger54 <kingdavidaut@gmail.com>
2021-12-18 23:07:27 +0100

    dispel component

--- src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/spell/component/Dispel.java
+++ src/main/java/com/github/minecraftschurli/arsmagicalegacy/common/spell/component/Dispel.java
@@ -3,17 +3,34 @@ package com.github.minecraftschurli.arsmagicalegacy.common.spell.component;
 import com.github.minecraftschurli.arsmagicalegacy.api.spell.ISpell;
 import com.github.minecraftschurli.arsmagicalegacy.api.spell.ISpellModifier;
 import com.github.minecraftschurli.arsmagicalegacy.api.spell.SpellCastResult;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 
+import java.util.ArrayList;
 import java.util.List;
 
-//TODO
 public class Dispel extends AbstractComponent {
     @Override
     public SpellCastResult invoke(ISpell spell, LivingEntity caster, Level level, List<ISpellModifier> modifiers, EntityHitResult target, int index, int ticksUsed) {
+        if (target.getEntity() instanceof LivingEntity living) {
+            List<MobEffect> effects = new ArrayList<>();
+            int left = 6;
+            for (MobEffectInstance effect : living.getActiveEffects()) {
+                int amplifier = effect.getAmplifier() + 1;
+                if (left >= amplifier) {
+                    left -= amplifier;
+                    effects.add(effect.getEffect());
+                }
+            }
+            for (MobEffect effect : effects) {
+                living.removeEffect(effect);
+            }
+            return effects.size() == 0 ? SpellCastResult.EFFECT_FAILED : SpellCastResult.SUCCESS;
+        }
         return SpellCastResult.EFFECT_FAILED;
     }
 
