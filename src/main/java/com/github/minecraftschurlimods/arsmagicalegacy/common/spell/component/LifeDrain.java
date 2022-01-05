66baf843451f7d9cb517f7eb113b916023629def
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-01-05 14:32:51 +0100

    life drain component

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/spell/component/LifeDrain.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/spell/component/LifeDrain.java
@@ -1,8 +1,11 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.common.spell.component;
 
+import com.github.minecraftschurlimods.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurlimods.arsmagicalegacy.api.spell.ISpell;
 import com.github.minecraftschurlimods.arsmagicalegacy.api.spell.ISpellModifier;
 import com.github.minecraftschurlimods.arsmagicalegacy.api.spell.SpellCastResult;
+import com.github.minecraftschurlimods.arsmagicalegacy.common.init.AMSpellParts;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
@@ -10,10 +13,16 @@ import net.minecraft.world.phys.EntityHitResult;
 
 import java.util.List;
 
-//TODO
 public class LifeDrain extends AbstractComponent {
     @Override
     public SpellCastResult invoke(ISpell spell, LivingEntity caster, Level level, List<ISpellModifier> modifiers, EntityHitResult target, int index, int ticksUsed) {
+        if (target.getEntity() instanceof LivingEntity living) {
+            int damage = 2 + ArsMagicaAPI.get().getSpellHelper().countModifiers(modifiers, living.isInvertedHealAndHarm() ? AMSpellParts.HEALING.getId() : AMSpellParts.DAMAGE.getId()) * 2;
+            if (living.hurt(DamageSource.OUT_OF_WORLD, damage)) {
+                caster.heal(damage);
+            }
+            return SpellCastResult.SUCCESS;
+        }
         return SpellCastResult.EFFECT_FAILED;
     }
 
