ef92085be3b6b7fa480299d91434a778f058fad9
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-01-05 19:54:59 +0100

    blink component

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/spell/component/Blink.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/spell/component/Blink.java
@@ -1,19 +1,38 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.common.spell.component;
 
+import com.github.minecraftschurlimods.arsmagicalegacy.api.ArsMagicaAPI;
 import com.github.minecraftschurlimods.arsmagicalegacy.api.spell.ISpell;
 import com.github.minecraftschurlimods.arsmagicalegacy.api.spell.ISpellModifier;
 import com.github.minecraftschurlimods.arsmagicalegacy.api.spell.SpellCastResult;
+import com.github.minecraftschurlimods.arsmagicalegacy.common.init.AMSpellParts;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import java.util.List;
 
-//TODO
 public class Blink extends AbstractComponent {
     @Override
     public SpellCastResult invoke(ISpell spell, LivingEntity caster, Level level, List<ISpellModifier> modifiers, EntityHitResult target, int index, int ticksUsed) {
+        Entity entity = target.getEntity();
+        double oldX = entity.getX(), oldY = entity.getEyeY(), oldZ = entity.getZ();
+        for (int range = 12 + ArsMagicaAPI.get().getSpellHelper().countModifiers(modifiers, AMSpellParts.RANGE.getId()) * 12; range > 0; range--) {
+            double x = oldX + entity.getLookAngle().x() * range;
+            double y = oldY + entity.getLookAngle().y() * range;
+            double z = oldZ + entity.getLookAngle().z() * range;
+            BlockState lower = level.getBlockState(new BlockPos(x, y, z));
+            BlockState upper = level.getBlockState(new BlockPos(x, y + 1, z));
+            if (y >= level.getMinBuildHeight() && y <= level.getMaxBuildHeight() - 1 && level.getBlockState(new BlockPos(x, y, z)).isAir() && level.getBlockState(new BlockPos(x, y + 1, z)).isAir()) {
+                target.getEntity().moveTo(x, y, z);
+                return SpellCastResult.SUCCESS;
+            }
+        }
         return SpellCastResult.EFFECT_FAILED;
     }
 
