fb346c31c0330bc147bc3e8fd2902a5e11a930e8
Minecraftschurli <minecraftschurli@gmail.com>
2022-01-12 17:02:44 +0100

    add jdoc for IRiftHelper

--- src/api/java/com/github/minecraftschurlimods/arsmagicalegacy/api/magic/IRiftHelper.java
+++ src/api/java/com/github/minecraftschurlimods/arsmagicalegacy/api/magic/IRiftHelper.java
@@ -1,8 +1,17 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.api.magic;
 
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
 
+/**
+ * Interface for rift related helper methods.
+ */
 public interface IRiftHelper {
-    ItemStackHandler getRift(Player player);
+    /**
+     * Get the rift item handler for the given player.
+     *
+     * @param player the player to get the rift item handler for
+     * @return the rift item handler for the given player
+     */
+    IItemHandlerModifiable getRift(Player player);
 }
