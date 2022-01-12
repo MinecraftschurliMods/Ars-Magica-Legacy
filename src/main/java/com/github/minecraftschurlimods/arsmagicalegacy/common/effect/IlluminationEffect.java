62e1c87e8eb3ca4a69958d304e49d454cbcee2f3
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-01-12 21:21:08 +0100

    illumination entertainment?

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/effect/IlluminationEffect.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/effect/IlluminationEffect.java
@@ -1,9 +1,19 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.common.effect;
 
+import com.github.minecraftschurlimods.arsmagicalegacy.common.init.AMBlocks;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.LightLayer;
 
 public class IlluminationEffect extends AMMobEffect {
     public IlluminationEffect() {
         super(MobEffectCategory.BENEFICIAL, 0xffffbe);
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier) {
+        if (pLivingEntity.getLevel().getBrightness(LightLayer.BLOCK, pLivingEntity.blockPosition()) == 0) {
+            pLivingEntity.getLevel().setBlock(pLivingEntity.blockPosition(), AMBlocks.SPELL_LIGHT.get().defaultBlockState(), 3);
+        }
     }
 }
