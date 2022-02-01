da6e81b76aed0267e941539fda54321e6406fc93
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-02-01 15:40:49 +0100

    add nullable suppression

--- src/api/java/com/github/minecraftschurlimods/arsmagicalegacy/api/ArsMagicaAPI.java
+++ src/api/java/com/github/minecraftschurlimods/arsmagicalegacy/api/ArsMagicaAPI.java
@@ -169,6 +169,7 @@ public final class ArsMagicaAPI {
         void openSpellCustomizationGui(Level level, Player player, ItemStack stack);
     }
 
+    @SuppressWarnings("ConstantConditions")
     private static class StubArsMagicaAPI implements IArsMagicaAPI {
         private static final IArsMagicaAPI INSTANCE = new StubArsMagicaAPI();
 
