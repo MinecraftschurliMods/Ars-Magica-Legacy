ddc3c659c5f6be9b869965ebbeaf3f87665b2012
Minecraftschurli <minecraftschurli@gmail.com>
2022-05-07 18:33:08 +0200

    add dryad

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/entity/Dryad.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/entity/Dryad.java
@@ -1,5 +1,8 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.common.entity;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -8,9 +11,14 @@ import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.PanicGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BonemealableBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 
 public class Dryad extends PathfinderMob {
     public Dryad(EntityType<? extends PathfinderMob> type, Level level) {
@@ -18,15 +26,32 @@ public class Dryad extends PathfinderMob {
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return createMobAttributes().add(Attributes.MAX_HEALTH, 20D);
+        return createMobAttributes().add(Attributes.MAX_HEALTH, 20D).add(Attributes.MOVEMENT_SPEED, 0.2F);
     }
 
     @Override
     protected void registerGoals() {
         goalSelector.addGoal(0, new FloatGoal(this));
-        goalSelector.addGoal(1, new PanicGoal(this, 2D));
+        goalSelector.addGoal(1, new PanicGoal(this, 1.5D));
+        goalSelector.addGoal(3, new TemptGoal(this, 1.25D, Ingredient.of(ItemTags.SAPLINGS), false));
         goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1D));
         goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 6F));
         goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        if (!(level instanceof ServerLevel serverLevel)) return;
+        if (level.random.nextDouble() < 0.01) return;
+        for (final BlockPos pos : BlockPos.betweenClosedStream(AABB.ofSize(position(), 5, 5, 5)).toList()) {
+            BlockState state = level.getBlockState(pos);
+            if (!(state.getBlock() instanceof BonemealableBlock bonemealableBlock)) continue;
+            if (!bonemealableBlock.isValidBonemealTarget(level, pos, state, false)) continue;
+            if (bonemealableBlock.isBonemealSuccess(serverLevel, serverLevel.random, pos, state)) {
+                bonemealableBlock.performBonemeal(serverLevel, serverLevel.random, pos, state);
+            }
+            break;
+        }
     }
 }
--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/init/AMEntities.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/init/AMEntities.java
@@ -39,8 +39,8 @@ public interface AMEntities {
     RegistryObject<EntityType<LifeGuardian>>      LIFE_GUARDIAN      = ENTITIES.register("life_guardian",      () -> EntityType.Builder.of(LifeGuardian::new,      MobCategory.MONSTER).clientTrackingRange(8).sized(    1,     2).build("life_guardian"));
     RegistryObject<EntityType<ArcaneGuardian>>    ARCANE_GUARDIAN    = ENTITIES.register("arcane_guardian",    () -> EntityType.Builder.of(ArcaneGuardian::new,    MobCategory.MONSTER).clientTrackingRange(8).sized(    1,     3).build("arcane_guardian"));
     RegistryObject<EntityType<EnderGuardian>>     ENDER_GUARDIAN     = ENTITIES.register("ender_guardian",     () -> EntityType.Builder.of(EnderGuardian::new,     MobCategory.MONSTER).clientTrackingRange(8).sized(    1,     3).build("ender_guardian"));
-    RegistryObject<EntityType<Dryad>>             DRYAD              = ENTITIES.register("dryad",              () -> EntityType.Builder.of(Dryad::new,             MobCategory.MONSTER).clientTrackingRange(8).sized(    5,     2).build("dryad"));
-    RegistryObject<EntityType<Mage>>              MAGE               = ENTITIES.register("mage",               () -> EntityType.Builder.of(Mage::new,              MobCategory.MONSTER).clientTrackingRange(8).sized(    5,     2).build("mage"));
+    RegistryObject<EntityType<Dryad>>             DRYAD              = ENTITIES.register("dryad",              () -> EntityType.Builder.of(Dryad::new,             MobCategory.AMBIENT).clientTrackingRange(8).sized(    1,     2).build("dryad"));
+    RegistryObject<EntityType<Mage>>              MAGE               = ENTITIES.register("mage",               () -> EntityType.Builder.of(Mage::new,              MobCategory.MONSTER).clientTrackingRange(8).sized(    1,     2).build("mage"));
     RegistryObject<EntityType<ManaCreeper>>       MANA_CREEPER       = ENTITIES.register("mana_creeper",       () -> EntityType.Builder.of(ManaCreeper::new,       MobCategory.MONSTER).clientTrackingRange(8).sized( 0.6F,  1.7F).build("mana_creeper"));
 
     /**
