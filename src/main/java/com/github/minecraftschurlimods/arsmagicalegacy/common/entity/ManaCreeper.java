5e44b1514bd5dfd70c2c829f38920387e41f7ca6
IchHabeHunger54 <kingdavidaut@gmail.com>
2022-08-30 17:22:35 +0200

    fix mana creeper goals

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/entity/ManaCreeper.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/entity/ManaCreeper.java
@@ -3,7 +3,6 @@ package com.github.minecraftschurlimods.arsmagicalegacy.common.entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.Level;
 
@@ -14,12 +13,6 @@ public class ManaCreeper extends Creeper {
 
     public static AttributeSupplier.Builder createAttributes() {
         return createMonsterAttributes().add(Attributes.MOVEMENT_SPEED, 0.25D);
-    }
-
-    @Override
-    protected void registerGoals() {
-        super.registerGoals();
-        goalSelector.getAvailableGoals().removeIf(goal -> goal.getGoal() instanceof AvoidEntityGoal<?>);
     }
 
     @Override
