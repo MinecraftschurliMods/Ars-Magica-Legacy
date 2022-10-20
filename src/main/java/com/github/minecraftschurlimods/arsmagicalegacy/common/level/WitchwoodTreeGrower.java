235907eb9a9745404c2e22676a7c2e9ac004d4f9
Minecraftschurli <minecraftschurli@gmail.com>
2022-10-20 19:57:13 +0200

    use new forge provided method to get tree feature holder in grower

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/level/WitchwoodTreeGrower.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/level/WitchwoodTreeGrower.java
@@ -1,14 +1,17 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.common.level;
 
 import com.github.minecraftschurlimods.arsmagicalegacy.api.ArsMagicaAPI;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractMegaTreeGrower;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraftforge.server.ServerLifecycleHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class WitchwoodTreeGrower extends AbstractMegaTreeGrower {
@@ -20,7 +23,13 @@ public class WitchwoodTreeGrower extends AbstractMegaTreeGrower {
 
     @Nullable
     @Override
+    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredMegaFeature(ServerLevel level, ChunkGenerator chunkGenerator, BlockPos pos, BlockState state, RandomSource random) {
+        return level.registryAccess().registryOrThrow(Registry.CONFIGURED_FEATURE_REGISTRY).getOrCreateHolderOrThrow(ResourceKey.create(Registry.CONFIGURED_FEATURE_REGISTRY, new ResourceLocation(ArsMagicaAPI.MOD_ID, "witchwood_tree")));
+    }
+
+    @Nullable
+    @Override
     protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredMegaFeature(RandomSource pRandom) {
-        return ServerLifecycleHooks.getCurrentServer().registryAccess().registryOrThrow(Registry.CONFIGURED_FEATURE_REGISTRY).getOrCreateHolderOrThrow(ResourceKey.create(Registry.CONFIGURED_FEATURE_REGISTRY, new ResourceLocation(ArsMagicaAPI.MOD_ID, "witchwood_tree"))); // TODO: Change when MinecraftForge/MinecraftForge#8956 gets merged
+        return null;
     }
 }
