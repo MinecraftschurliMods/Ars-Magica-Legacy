0cf05a11a24a283c8b5d23c690cbf8011f27afe8
Minecraftschurli <minecraftschurli@gmail.com>
2022-12-21 22:36:16 +0100

    fix mana creeper spawning

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/handler/EventHandler.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/handler/EventHandler.java
@@ -66,9 +66,11 @@ import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.DefaultAttributes;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -79,6 +81,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.LecternBlock;
 import net.minecraft.world.level.block.entity.LecternBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.featuresize.ThreeLayersFeatureSize;
@@ -171,10 +174,17 @@ public final class EventHandler {
     }
 
     private static void setup(FMLCommonSetupEvent event) {
-        registerBrewingRecipes();
-        registerSpellIngredientTypes();
-        AMCriteriaTriggers.register();
+        event.enqueueWork(() -> {
+            registerBrewingRecipes();
+            registerSpellIngredientTypes();
+            AMCriteriaTriggers.register();
+            registerSpawnPlacements();
+        });
         CompatManager.init(event);
+    }
+
+    private static void registerSpawnPlacements() {
+        SpawnPlacements.register(AMEntities.MANA_CREEPER.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Monster::checkMonsterSpawnRules);
     }
 
     private static void registerBrewingRecipes() {
