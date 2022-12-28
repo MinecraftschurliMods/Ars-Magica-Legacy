ebc6dbb8c37bd5d857af8ad036e6e6f8a8f8988e
Minecraftschurli <minecraftschurli@gmail.com>
2022-12-28 13:36:12 +0100

    implement 1.19 specific changes

--- src/data/java/com/github/minecraftschurlimods/arsmagicalegacy/data/AMWorldgenProvider.java
+++ src/data/java/com/github/minecraftschurlimods/arsmagicalegacy/data/AMWorldgenProvider.java
@@ -20,12 +20,9 @@ import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.LakeFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.ThreeLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.DarkOakFoliagePlacer;
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.DarkOakTrunkPlacer;
 import net.minecraft.world.level.levelgen.placement.BiomeFilter;
 import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
@@ -35,13 +32,12 @@ import net.minecraft.world.level.levelgen.placement.RarityFilter;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
-import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
 import java.util.OptionalInt;
 import java.util.function.Supplier;
 
-public class AMWorldgenProvider extends WorldgenProvider {
+class AMWorldgenProvider extends WorldgenProvider {
     AMWorldgenProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
         super(generator, existingFileHelper, ArsMagicaAPI.MOD_ID);
     }
@@ -60,7 +56,6 @@ public class AMWorldgenProvider extends WorldgenProvider {
         cf("tarma_root", flower(64, AMBlocks.TARMA_ROOT));
         cf("wakebloom", flower(64, AMBlocks.WAKEBLOOM));
         cf("witchwood_tree", tree(AMBlocks.WITCHWOOD_LOG, new DarkOakTrunkPlacer(9, 3, 1), AMBlocks.WITCHWOOD_LEAVES, new DarkOakFoliagePlacer(ConstantInt.of(1), ConstantInt.of(1)), new ThreeLayersFeatureSize(1, 2, 1, 1, 2, OptionalInt.empty())));
-        cf("liquid_essence_lake", feature(Feature.LAKE, new LakeFeature.Configuration(BlockStateProvider.simple(AMBlocks.LIQUID_ESSENCE.get()), BlockStateProvider.simple(Blocks.GRASS_BLOCK))));
     }
 
     @Override
@@ -78,7 +73,6 @@ public class AMWorldgenProvider extends WorldgenProvider {
         pf("wakebloom", flowerPlacement(cf("wakebloom"), 32));
         pf("witchwood_tree", treePlacement(cf("witchwood_tree"), AMBlocks.WITCHWOOD_SAPLING));
         pf("trees_witchwood", treeVegetation(cf("witchwood_tree"), PlacementUtils.countExtra(1, 0.1F, 0), 8, AMBlocks.WITCHWOOD_SAPLING));
-        pf("liquid_essence_lake", placement(cf("liquid_essence_lake"), RarityFilter.onAverageOnceEvery(500), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE, BiomeFilter.biome()));
     }
 
     @Override
@@ -88,14 +82,13 @@ public class AMWorldgenProvider extends WorldgenProvider {
         bm("topaz_ore", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(bt(BiomeTags.IS_OVERWORLD), HolderSet.direct(pf("topaz_ore")), GenerationStep.Decoration.UNDERGROUND_ORES));
         bm("topaz_extra_ore", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), bt(BiomeTags.IS_MOUNTAIN)), HolderSet.direct(pf("topaz_ore_extra")), GenerationStep.Decoration.UNDERGROUND_ORES));
         bm("sunstone_ore", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(bt(BiomeTags.IS_NETHER), HolderSet.direct(pf("sunstone_ore")), GenerationStep.Decoration.UNDERGROUND_ORES));
-        bm("moonstone_meteoreite", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), bt(BiomeTags.IS_OCEAN)), HolderSet.direct(pf("moonstone_meteoreite")), GenerationStep.Decoration.LOCAL_MODIFICATIONS));
+        bm("moonstone_meteoreite", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), not(bt(BiomeTags.IS_OCEAN))), HolderSet.direct(pf("moonstone_meteoreite")), GenerationStep.Decoration.LOCAL_MODIFICATIONS));
         bm("aum", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), bt(BiomeTags.IS_FOREST)), HolderSet.direct(pf("aum")), GenerationStep.Decoration.VEGETAL_DECORATION));
         bm("cerublossom", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), or(bt(BiomeTags.IS_JUNGLE), bt(Tags.Biomes.IS_SWAMP))), HolderSet.direct(pf("cerublossom")), GenerationStep.Decoration.VEGETAL_DECORATION));
         bm("desert_nova", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), bt(Tags.Biomes.IS_SANDY)), HolderSet.direct(pf("desert_nova")), GenerationStep.Decoration.VEGETAL_DECORATION));
         bm("tarma_root", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), or(bt(BiomeTags.IS_MOUNTAIN), bt(BiomeTags.IS_HILL), bt(Tags.Biomes.IS_UNDERGROUND))), HolderSet.direct(pf("tarma_root")), GenerationStep.Decoration.VEGETAL_DECORATION));
         bm("wakebloom", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), or(bt(BiomeTags.IS_JUNGLE), bt(Tags.Biomes.IS_SWAMP))), HolderSet.direct(pf("wakebloom")), GenerationStep.Decoration.VEGETAL_DECORATION));
         bm("witchwood_tree", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(and(bt(BiomeTags.IS_OVERWORLD), bt(Tags.Biomes.IS_SPOOKY)), HolderSet.direct(pf("trees_witchwood")), GenerationStep.Decoration.VEGETAL_DECORATION));
-        bm("liquid_essence_lake", new ForgeBiomeModifiers.AddFeaturesBiomeModifier(bt(BiomeTags.IS_OVERWORLD), HolderSet.direct(pf("liquid_essence_lake")), GenerationStep.Decoration.LAKES));
         bm("dryad", ForgeBiomeModifiers.AddSpawnsBiomeModifier.singleSpawn(and(bt(BiomeTags.IS_OVERWORLD), bt(BiomeTags.IS_FOREST)), new MobSpawnSettings.SpawnerData(AMEntities.DRYAD.get(), 2, 15, 25)));
         bm("mana_creeper", ForgeBiomeModifiers.AddSpawnsBiomeModifier.singleSpawn(and(bt(BiomeTags.IS_OVERWORLD), not(b(Biomes.DEEP_DARK), bt(Tags.Biomes.IS_MUSHROOM))), new MobSpawnSettings.SpawnerData(AMEntities.MANA_CREEPER.get(), 10, 1, 4)));
     }
--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/client/gui/SpellRecipeScreen.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/client/gui/SpellRecipeScreen.java
@@ -32,7 +32,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.crafting.Ingredient;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
