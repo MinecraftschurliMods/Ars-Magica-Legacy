35b8c27c579dcdf151bcd20d6cb7bdf79588b1e4
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-06-13 21:50:42 +0200

    1.19 port of #393

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/init/AMSkillPoints.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/common/init/AMSkillPoints.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.ApiStatus.NonExtendable;
 
 @NonExtendable
 public interface AMSkillPoints {
+    RegistryObject<SkillPoint> NONE  = AMRegistries.SKILL_POINTS.register("none",  () -> new SkillPoint(0x7f7f7f, -1, -1));
     RegistryObject<SkillPoint> BLUE  = AMRegistries.SKILL_POINTS.register("blue",  () -> new SkillPoint(0x0000ff, 0,  1));
     RegistryObject<SkillPoint> GREEN = AMRegistries.SKILL_POINTS.register("green", () -> new SkillPoint(0x00ff00, 10, 2));
     RegistryObject<SkillPoint> RED   = AMRegistries.SKILL_POINTS.register("red",   () -> new SkillPoint(0xff0000, 20, 3));
