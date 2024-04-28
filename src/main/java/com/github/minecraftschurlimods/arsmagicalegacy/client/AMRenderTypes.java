e69b5a1fb665db373718c9b9ccb72813a67a1d72
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-04-28 14:15:11 +0200

    move the color picker state into a singleton class

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/client/AMRenderTypes.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/client/AMRenderTypes.java
@@ -1,21 +1,13 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.client;
 
+import com.github.minecraftschurlimods.arsmagicalegacy.client.gui.inscriptiontable.colorpicker.ColorPickerState;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 
-public class AMRenderTypes {
+public final class AMRenderTypes {
     public static final RenderStateShard.ShaderStateShard COLOR_WHEEL_SHADER = new RenderStateShard.ShaderStateShard(AMShaders::getColorWheelShader);
-    public static float colorWheelCenterX, colorWheelCenterY, colorWheelRadius, colorWheelBrightness; // TODO: This is incredibly hacky, find a better way
-
-    public static void setColorWheel(float cX, float cY, float radius, float brightness) {
-        colorWheelCenterX = cX;
-        colorWheelCenterY = cY;
-        colorWheelRadius = radius;
-        colorWheelBrightness = brightness;
-    }
-
     public static final RenderType COLOR_WHEEL = RenderType.create(
             "color_wheel",
             DefaultVertexFormat.POSITION_COLOR,
@@ -29,9 +21,10 @@ public class AMRenderTypes {
                     .setTransparencyState(RenderStateShard.TRANSLUCENT_TRANSPARENCY)
                     .setDepthTestState(RenderStateShard.LEQUAL_DEPTH_TEST)
                     .setLayeringState(new RenderStateShard.LayeringStateShard("set_uniforms", () -> {
-                        AMShaders.setUniform("center", colorWheelCenterX, colorWheelCenterY);
-                        AMShaders.setUniform("radius", colorWheelRadius);
-                        AMShaders.setUniform("brightness", colorWheelBrightness);
+                        ColorPickerState state = ColorPickerState.get();
+                        AMShaders.setUniform("center", state.getCenterX(), state.getCenterY());
+                        AMShaders.setUniform("radius", state.getRadius());
+                        AMShaders.setUniform("brightness", state.getBrightness());
                     }, () -> {}))
                     .createCompositeState(false)
     );
