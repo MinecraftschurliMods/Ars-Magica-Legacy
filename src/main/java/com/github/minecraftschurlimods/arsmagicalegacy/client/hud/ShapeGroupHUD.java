af34e9c59ed5ef0585a5569e4dbeb39cc9e36ec0
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-05-09 13:03:01 +0200

    add configuration values for shape group hud

--- src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/client/hud/ShapeGroupHUD.java
+++ src/main/java/com/github/minecraftschurlimods/arsmagicalegacy/client/hud/ShapeGroupHUD.java
@@ -1,12 +1,13 @@
 package com.github.minecraftschurlimods.arsmagicalegacy.client.hud;
 
+import com.github.minecraftschurlimods.arsmagicalegacy.Config;
 import com.github.minecraftschurlimods.betterhudlib.HUDElement;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 
 public final class ShapeGroupHUD extends HUDElement {
     public ShapeGroupHUD() {
-        super(AnchorX.CENTER, AnchorY.CENTER, 0, 0, 0, 0);
+        super(Config.CLIENT.SHAPE_GROUP_ANCHOR_X, Config.CLIENT.SHAPE_GROUP_ANCHOR_Y, Config.CLIENT.SHAPE_GROUP_X::get, Config.CLIENT.SHAPE_GROUP_Y::get, () -> 180, () -> 34);
     }
 
     @Override
