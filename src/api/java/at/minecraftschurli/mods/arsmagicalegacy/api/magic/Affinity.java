0d1989810bdef6d54fa2280c8faf0f5667e2bd20
IchHabeHunger54 <kingdavidaut@gmail.com>
2026-08-23 15:13:59 +0200

    fix recursive Affinity#toString

--- src/api/java/at/minecraftschurli/mods/arsmagicalegacy/api/magic/Affinity.java
+++ src/api/java/at/minecraftschurli/mods/arsmagicalegacy/api/magic/Affinity.java
@@ -15,9 +15,12 @@ import net.minecraft.resources.RegistryFileCodec;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Util;
+import net.neoforged.neoforge.common.extensions.IHolderExtension;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /// Represents an affinity.
 ///
@@ -59,6 +62,21 @@ public record Affinity(Holder<Affinity> directOpposite, List<Holder<Affinity>> m
     /// @param particle       The [ParticleOptions] to associate with the affinity.
     public Affinity(Holder<Affinity> directOpposite, List<Holder<Affinity>> majorOpposites, List<Holder<Affinity>> minorOpposites, List<Holder<Affinity>> adjacents, int color, double index, Holder<SoundEvent> castSound, Holder<SoundEvent> loopSound, ParticleOptions particle) {
         this(directOpposite, majorOpposites, minorOpposites, adjacents, color, index, Optional.of(castSound), Optional.of(loopSound), particle);
+    }
+
+    @Override
+    public String toString() {
+        return "Affinity{" +
+            "directOpposite=" + directOpposite.getKey() +
+            ", majorOpposites=[" + majorOpposites.stream().map(IHolderExtension::getKey).filter(Objects::nonNull).map(ResourceKey::toString).collect(Collectors.joining(",")) +
+            "], minorOpposites=[" + minorOpposites.stream().map(IHolderExtension::getKey).filter(Objects::nonNull).map(ResourceKey::toString).collect(Collectors.joining(",")) +
+            "], adjacents=[" + adjacents.stream().map(IHolderExtension::getKey).filter(Objects::nonNull).map(ResourceKey::toString).collect(Collectors.joining(",")) +
+            "], color=" + color +
+            ", index=" + index +
+            ", castSound=" + castSound +
+            ", loopSound=" + loopSound +
+            ", particle=" + particle +
+            '}';
     }
 
     /// @param holder The affinity [Holder] to query.
